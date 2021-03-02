1d932d0b3677403ff8d72cbc5ded73ff06b702ea
Bilal <sbilalahmed95@gmail.com>
2021-03-02 22:48:24 +0400

    stories added

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import MessageSender from '../MessageSender/MessageSender'
 import StoryReel from '../StoryReel/StoryReel'
-
+import './Feed.css'
 const Feed = () => {
     return (
         <div className="feed">
--- src/components/StoryReel/StoryReel.js
+++ src/components/StoryReel/StoryReel.js
@@ -11,6 +11,11 @@ const StoryReel = () => {
                 title="Megan Fox"
             />
             <Story
+            profileSrc="https://instagram.fauh1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/14726263_186588948448257_4356541072436887552_a.jpg?tp=1&_nc_ht=instagram.fauh1-1.fna.fbcdn.net&_nc_ohc=asxnEBcQcqQAX_K1DJW&oh=e7939f60da260461fa5f91f54291e008&oe=6068BAAE"
+            image="https://instagram.fauh1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/127233402_222309845933892_3125944430051239344_n.jpg?tp=1&_nc_ht=instagram.fauh1-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=733BKe1QzyEAX8hDSgt&oh=bb27ffcf050a71abd059a6005d1517d7&oe=60664361"
+            title="Atif Aslam"
+            />
+            <Story
                 profileSrc="https://instagram.ffjr1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/125430666_410380796818229_4185377368353646577_n.jpg?tp=1&_nc_ht=instagram.ffjr1-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=S1lNHfai8sEAX-3SOrC&oh=20386fdf69b9dde17600bd5816012da7&oe=6069125F"
                 image="https://instagram.ffjr1-4.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/118853242_2755521401438131_6175863926407644214_n.jpg?tp=1&_nc_ht=instagram.ffjr1-4.fna.fbcdn.net&_nc_cat=107&_nc_ohc=flVqZcofO8sAX-EJ2fw&oh=d73017c07d3f680d27268261edbb583e&oe=6066D994"
                 title="Hailee Steinfeld"
@@ -20,7 +25,11 @@ const StoryReel = () => {
                 image="https://instagram.ffjr1-5.fna.fbcdn.net/v/t51.2885-15/e35/c0.180.1440.1440a/s240x240/152445403_4022420774480782_1669638331238376752_n.jpg?tp=1&_nc_ht=instagram.ffjr1-5.fna.fbcdn.net&_nc_cat=110&_nc_ohc=RAfX3ukk8yQAX9OUnO7&oh=d899de6e88a80a02a10dec0ebc6a8787&oe=6068B828"
                 title="Marz.z.z"
             />
-            <Story />
+            <Story
+            profileSrc="https://instagram.fauh1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/132197016_214462623590305_6641430723485871092_n.jpg?tp=1&_nc_ht=instagram.fauh1-1.fna.fbcdn.net&_nc_ohc=xVcLctWe-ooAX9mjeb-&oh=4b31db0e5ec74c1d783cc76fa606ea6f&oe=60684C93"
+            image="https://instagram.fauh1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/150391572_2566603803638802_7584376247762659577_n.jpg?tp=1&_nc_ht=instagram.fauh1-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=r7dHNDHgDYAAX8wpxaF&oh=e399ed47619170a36483fd6efb9db7b3&oe=60697E2C"
+            title="Gal Gadot"
+            />
         </div>
     )
 }
